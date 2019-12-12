56223e7ab7fcd80abc92b45887fae96b505cd8c6
Roger Lipscombe <roger@differentpla.net>
2019-12-12 18:21:03 +0000

    Initial creation

---
+++ lib/dockerns.ex
@@ -0,0 +1,18 @@
+defmodule Dockerns do
+  @moduledoc """
+  Documentation for Dockerns.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dockerns.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
